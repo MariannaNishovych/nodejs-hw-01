887612a99a15131b066229f01b74d350f647ab92
Marianna Nishovych <nisovicmarianna@gmail.com>
2025-01-13 14:53:12 +0200

    add first commit

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,33 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+// import { writeContacts } from '../utils/writeContacts.js';
+import fs from 'node:fs/promises';
+
+export const generateContacts = async (number) => {
+try {
+    const existingContacts = await readContacts();
+
+    const newContact = [];
+
+    for (let i = 0; i < number; i++) {
+    const contact = createFakeContact();
+    newContact.push(contact);
+    }
+
+    const updatedContacts = [...existingContacts, ...newContact];
+
+    await fs.writeFile( PATH_DB,
+        JSON.stringify(updatedContacts, null, 2),
+        'utf-8',
+    );
+    console.log('Successful contacts generete!');
+} catch (error) {
+    console.error(
+    'Oops, an error occurred while genereting of contacts',
+    error,
+    );
+}
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node: fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+    const data = JSON.stringify(updatedContacts);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts successfully written to file!');
+    } catch (error) {
+    console.error('Oops, an error occurred while written a file', error);
+    }
+};
